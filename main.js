fd63b5444f663946d08edd09992e6b47c60829af
Aben <126703861+abengl@users.noreply.github.com>
2024-07-30 11:16:52 -0500

    update: main.js

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
 import { Card } from "./Card.js";
 import { fetchData } from "./apiService.js";
 
-function displayData(data) {}
-
-function displayError(error) {}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const url =
-    "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json";
+function displayData(url, attribute = null) {
+  const cardsContainer = document.querySelector(".root__content__cards");
+  cardsContainer.innerHTML = "";
 
   fetchData(url)
     .then((data) => {
+      if (attribute) {
+        data = data.filter((item) => item.available === attribute);
+      }
       console.log(data);
-      const cardsContainer = document.querySelector(".root__content__cards");
-
       data.forEach((item) => {
-        console.log(item);
         const card = Card(item);
         cardsContainer.appendChild(card);
       });
@@ -23,4 +19,21 @@ document.addEventListener("DOMContentLoaded", () => {
     .catch((error) => {
       console.error("Error fetching and rendering data: ", error);
     });
+}
+
+function displayError(error) {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const allProductsButton = document.querySelector("#all__products");
+  const availableButton = document.querySelector("#available");
+  const url =
+    "https://raw.githubusercontent.com/devchallenges-io/web-project-ideas/main/front-end-projects/data/simple-coffee-listing-data.json";
+
+  allProductsButton.addEventListener("click", () => {
+    displayData(url);
+  });
+
+  availableButton.addEventListener("click", () => {
+    displayData(url, true);
+  });
 });
